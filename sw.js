bf927364dfb2875853d8ab472a2e504603f77f17
C.Silva <csdesweb@gmail.com>
2026-01-05 22:34:51 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,24 @@
-self.addEventListener("install",e=>{
+const CACHE_NAME = "faststile-v3.1";
+const FILES_TO_CACHE = ["./","index.html","manifest.json","icons/icon-192.png","icons/icon-512.png"];
+
+self.addEventListener("install", e => {
  e.waitUntil(
-  caches.open("faststile-v3").then(c=>
-   c.addAll(["./","index.html","manifest.json"])
-  )
+   caches.open(CACHE_NAME).then(c => c.addAll(FILES_TO_CACHE))
  );
+ self.skipWaiting();
 });
 
-self.addEventListener("fetch",e=>{
+self.addEventListener("activate", e => {
+ e.waitUntil(
+   caches.keys().then(keys => Promise.all(keys.map(k => {
+     if(k!==CACHE_NAME) return caches.delete(k);
+   })))
+ );
+ self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
  e.respondWith(
-  caches.match(e.request).then(r=>r||fetch(e.request))
+   caches.match(e.request).then(r => r || fetch(e.request))
  );
 });
