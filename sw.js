e1c0d373a8ffac39e72b419093c2e36d629eb989
C.Silva <csdesweb@gmail.com>
2026-01-06 19:39:07 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,11 @@
-const cacheName="faststile-v52";
+const cacheName="faststile-v53";
 self.addEventListener("install",e=>{
-e.waitUntil(caches.open(cacheName).then(c=>c.addAll(["./"])));
+e.waitUntil(
+caches.open(cacheName).then(c=>c.addAll(["./"]))
+);
 });
 self.addEventListener("fetch",e=>{
-e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+e.respondWith(
+caches.match(e.request).then(r=>r||fetch(e.request))
+);
 });
