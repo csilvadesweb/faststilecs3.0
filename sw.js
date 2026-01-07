a8564c40fbbb3e9e7af6e9d9ef2d7927fc7e1c7d
C.Silva <csdesweb@gmail.com>
2026-01-07 11:07:51 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,20 @@
-self.addEventListener('install',e=>{
- e.waitUntil(caches.open('fs').then(c=>c.addAll(['./','index.html','style.css','script.js'])))
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('fs-cache').then(cache => cache.addAll([
+      './',
+      './index.html',
+      './style.css',
+      './script.js',
+      './manifest.json',
+      './privacy.html',
+      './icons/icon-192.png',
+      './icons/icon-512.png'
+    ]))
+  );
 });
-self.addEventListener('fetch',e=>{
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
